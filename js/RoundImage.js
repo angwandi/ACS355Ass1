3c37b6fcb16e2d80315c7cbdfc916998fefd21d3
Innocent Angwadia <demsdema217@gmail.com>
2019-03-12 14:14:58 +0300

    Add eval function js

--- js/RoundImage.js
+++ js/RoundImage.js
@@ -1,10 +1,15 @@
-let image = document.querySelector("#logo")
+let image = document.querySelector("#logo");
 image.onclick = toggleClass;
+image.onclick = window.alert(evalString());
 
-function toggleClass(){
-    if(image.className === 'logo'){
+function toggleClass() {
+    if (image.className === 'logo') {
         image.className = ''
     } else {
         image.className = 'logo'
     }
+}
+
+function evalString() {
+    eval("2 + 5")
 }
